748e0017eb6197b6f50f2dcc4987c3a708971e7f
Rob2Tracy <awppark1@protonmail.com>
2020-01-23 19:56:58 -0500

    Update Main.py

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -5,8 +5,8 @@ import censys.ipv4
 
 
 API_URL = "https://censys.io/api/v1"
-UID = "a4e47672-8df9-4bd3-a935-7409ff4c333c"
-SECRET = "jqL3H9ZhbGfTrqCGRSCqWdNJfXOunpzv"
+UID = ""
+SECRET = ""
 hostIPS = []
 hostDetailed = []
 
