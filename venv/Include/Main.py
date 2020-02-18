6d213ae2b7ebcace1b64eb5e67fbde063e5d8bbb
Rob2Tracy <awppark1@protonmail.com>
2020-02-18 09:57:57 -0500

    Update Main.py

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -8,8 +8,8 @@ from Model.Host import Host
 
 
 API_URL = "https://censys.io/api/v1"
-UID = "a4e47672-8df9-4bd3-a935-7409ff4c333c"
-SECRET = "jqL3H9ZhbGfTrqCGRSCqWdNJfXOunpzv"
+UID = ""
+SECRET = ""
 hostIPS = []
 hostDetailed = []
 
